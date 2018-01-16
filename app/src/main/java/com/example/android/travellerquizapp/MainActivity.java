68a1d870150fdbd558e102bf0870bfd2bb9c61fa
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-01-16 20:12:03 +0000

    no needed imports

--- app/src/main/java/com/example/android/travellerquizapp/MainActivity.java
+++ app/src/main/java/com/example/android/travellerquizapp/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
